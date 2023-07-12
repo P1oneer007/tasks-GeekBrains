e332648abe38d058a284570f131e13d922b143b0
pioneer <Leka-07@bk.ru>
2023-07-12 13:35:47 +0300

    release 01

---
+++ tasks1/lesson_1/lesson_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var UserName = Environment.UserName;
+            Console.WriteLine($"Привет, {UserName}, сегодня {System.DateTime.Now:dd.MM.yyyy}");
+        }
+    }
+}
