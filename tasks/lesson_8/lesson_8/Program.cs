cd730583712d19497d375b5c5a1d8c09eb97b9ee
P1oneer007 <139325174+P1oneer007@users.noreply.github.com>
2023-07-14 10:41:14 +0300

    Add files via upload

---
+++ tasks/lesson_8/lesson_8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Выводим приветствие из настроек приложения
+            string greeting = ConfigurationManager.AppSettings["Greeting"];
+            Console.WriteLine(greeting);
+
+            // Запрашиваем у пользователя данные
+            Console.Write("Введите Ваше имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите Ваш возраст: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите Ваш род деятельности: ");
+            string occupation = Console.ReadLine();
+
+            // Сохраняем данные в настройках
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings["Name"].Value = name;
+            config.AppSettings.Settings["Age"].Value = age.ToString();
+            config.AppSettings.Settings["Occupation"].Value = occupation;
+            config.Save(ConfigurationSaveMode.Modified);
+
+            Console.WriteLine("Данные сохранены.");
+
+            // Выводим данные при следующем запуске
+            Console.WriteLine("Имя: " + config.AppSettings.Settings["Name"].Value);
+            Console.WriteLine("Возраст: " + config.AppSettings.Settings["Age"].Value);
+            Console.WriteLine("Род деятельности: " + config.AppSettings.Settings["Occupation"].Value);
+
+            Console.ReadLine();
+        }
+    }
