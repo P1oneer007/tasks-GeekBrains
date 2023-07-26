e571b8dbd0379bcf57d95c60d3b2b19830d723fb
pioneer <Leka-07@bk.ru>
2023-07-26 16:16:46 +0300

    1

---
+++ source/repos/AvaloniaApp1/AvaloniaApp1/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaApp1.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
